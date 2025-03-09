5f339a698e956a10bf3a469e0edb095d1cf3e106
johnkearns617 <79102162+johnkearns617@users.noreply.github.com>
2025-03-09 12:20:14 -0400

    update

--- XDate/app.R
+++ XDate/app.R
@@ -7,27 +7,37 @@
 #    https://shiny.posit.co/
 #
 
+# eventually i will need an RData or some other file that gives all the historic information that I need
+
 library(shiny)
+library(plotly)
+
+setwd("/Users/johnkearns/Documents/GitHub/XDate_Estimation")
+
+load("Data/Processing/image_saves/data_asof_2025-03-05.RData")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("XDate Estimation Charts"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            h5("This is where model explanation and links would go")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           tabsetPanel(
+             tabPanel("XDate",
+                      plotlyOutput('xdate_chart')),
+             tabPanel("Government Deficits",
+                      ),
+             tabPanel("GDP Nowcast",
+                      )
+           )
         )
     )
 )
@@ -35,15 +45,13 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$xdate_chart <- renderPlotly({
+      ggplotly(ggplot(my_chart %>% filter(date<="2025-09-30") %>% mutate(label=paste0("$",round(running_bal,2),"B")),aes(x=date)) + 
+        geom_ribbon(aes(ymin=running_bal_lower,ymax=running_bal_upper),alpha=.3) +
+        geom_line(aes(y=running_bal)) +
+        theme_bw() +
+        labs(x="",y="Fiscal Space Remaining ($B)")
+        )
     })
 }
 
