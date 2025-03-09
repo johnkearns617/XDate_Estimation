21bd151af9cf55b8ab303f1852ad55f4881cc621
johnkearns617 <79102162+johnkearns617@users.noreply.github.com>
2025-03-09 15:26:51 -0400

    fixing file refs

--- Do/master.R
+++ Do/master.R
@@ -18,24 +18,24 @@ reticulate::py_require(c('google-api-python-client',
 
 reticulate::py_run_file("Do/1a_pull_trends_data.py")
 
-source('0_model_functions.R')
+source('Do/0_model_functions.R')
 
-source('1_clean_google_trends.R')
+source('Do/1_clean_google_trends.R')
 
-source('2_assemble_econ_data.R')
+source('Do/2_assemble_econ_data.R')
 
-source('3_construct_dataset_with_data_lags.R')
+source('Do/3_construct_dataset_with_data_lags.R')
 
-source('4_feature_imputation.R')
+source('Do/4_feature_imputation.R')
 
-source('5_construct_GT_index.R')
+source('Do/5_construct_GT_index.R')
 
-source('7_gt_deficit_modelling.R')
+source('Do/7_gt_deficit_modelling.R')
 
-source('8_daily_model.R')
+source('Do/8_daily_model.R')
 
-source('9_XDate_estimation.R')
+source('Do/9_XDate_estimation.R')
 
-source('10_make_charts.R')
+source('Do/10_make_charts.R')
 
 
